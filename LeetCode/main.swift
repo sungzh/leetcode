741e55d603bdccd7042f52d0fbd2975cd1e4ea0f
sungzh <sdsxsgz@gmail.com>
2016-07-12 10:53:25 +0800

    Initial Commit

---
+++ LeetCode/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  LeetCode
+//
+//  Created by SunGuoZheng on 7/12/16.
+//  Copyright © 2016 SunGuoZheng. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
